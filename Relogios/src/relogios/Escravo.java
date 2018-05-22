02a9824fae3d561c5ed2e8c54fdf566321172dc9
Hamilton Junior <hamilton.junior96@yahoo.com.br>
2018-05-21 21:26:21 -0300

    Add files via upload

--- Relogios/src/relogios/Escravo.java
+++ Relogios/src/relogios/Escravo.java
@@ -5,6 +5,17 @@
  */
 package relogios;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.nio.ByteBuffer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Hamilton
@@ -18,6 +29,27 @@ public class Escravo {
      * @param tempo 
      */
     public void executar(String ipMestre, int tempo){
-        
+        try {
+            BufferedReader inFromUser =
+                    new BufferedReader(new InputStreamReader(System.in));
+            DatagramSocket clientSocket = new DatagramSocket();
+            InetAddress IPAddress = InetAddress.getByName(ipMestre.substring(0, ipMestre.lastIndexOf(":")));
+            int porta = Integer.parseInt(ipMestre.substring(ipMestre.lastIndexOf(":"), ipMestre.length()));
+            byte[] sendData = new byte[1024];
+            byte[] receiveData = new byte[1024];
+            System.out.println("Horário Atual: " + tempo);
+            sendData = ByteBuffer.allocate(4).putInt(tempo).array();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, porta);
+            clientSocket.send(sendPacket);
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            clientSocket.receive(receivePacket);
+            String novoTempo = new String(receivePacket.getData());
+            System.out.println("Horário Ajustado: " + novoTempo);
+            clientSocket.close();
+        } catch (SocketException ex) {
+            Logger.getLogger(Escravo.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Escravo.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
--- Relogios/src/relogios/Mestre.java
+++ Relogios/src/relogios/Mestre.java
@@ -5,45 +5,102 @@
  */
 package relogios;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Hamilton
  */
 public class Mestre {
-    
+
     /**
-     * @author Hamilton
-     * Executa as funções do nó Mestre
-     * @param escravos - ips dos escravos 
+     * @author Hamilton Executa as funções do nó Mestre
+     * @param escravos - ips dos escravos
      * @param ipMestre - ip do nó mestre
      * @param tempo - relógio a ser inicializado
      * @param limite - limite de diferença tolerado entre os relógios
      */
-    public void executar(List<String> escravos, String ipMestre, int tempo, int limite ){
-        //Pega os horarios dos relógios
-        int horarioMestre = limite;
-        int[] horarios = {72,102,119};
-        //Variável contadora para usar no cálculo da média
-        int contador=0, soma=0, media;
-        //Calcula a média de diferença entre eles considerando o limite passado por parâmetro
-        for(int i = 0; i< horarios.length; i++){
-            //Se estiver dentro do intervalo permitido, incrementa contador e adiciona o horário na soma
-            if(horarios[i] - horarioMestre < tempo || horarios[i] - horarioMestre > tempo*-1){
-                contador++;
-                soma+=horarios[i];
-            }               
+    public void executar(List<String> escravos, String ipMestre, int tempo, int limite) {
+        try {
+            while (true) {
+                //Variável para receber os horários dos escravos
+                byte[] receiveData = new byte[1024];
+                //Variável para armazenar os valores de ajuste para os escravos
+                byte[] sendData = new byte[1024];
+//                List<DatagramSocket> sockets = new ArrayList<>();
+//                List<DatagramPacket> pacotes = new ArrayList<>();
+//                List<InetAddress> ips = new ArrayList<>();
+//                List<Integer> portas = new ArrayList<>();
+//                List<Integer> horarios = new ArrayList<>();
+
+                
+                DatagramPacket pacote;
+                InetAddress ip;
+                int porta;
+
+                //pega as informações vindas dos escravos
+//                for (String escravo : escravos) {
+//                    porta = Integer.parseInt(escravo.substring(escravo.indexOf(":"), escravo.length()));
+                
+//                    sockets.add(socket);
+
+                pacote = new DatagramPacket(receiveData, receiveData.length);
+//                    pacotes.add(pacote);
+               
+                ip = pacote.getAddress();
+                porta = pacote.getPort();
+                DatagramSocket socket=new DatagramSocket(porta);
+                socket = new DatagramSocket(porta);
+                socket.receive(pacote);
+//                    ips.add(ip);
+//                    portas.add(porta);
+//                }
+
+                //Pega os horarios dos relógios
+                int horarioMestre = limite;
+                int contador = 0, soma = 0, media;
+                String horarioNaoFormatado;
+                int horas;
+                int minutos;
+                int horarioConvertido;
+                //calcula a média 
+//                for (DatagramPacket pack : pacotes) {
+                horarioNaoFormatado = new String(pacote.getData());
+                horas = Integer.parseInt(horarioNaoFormatado.substring(0, horarioNaoFormatado.indexOf(":")));
+                minutos = Integer.parseInt(horarioNaoFormatado.substring(horarioNaoFormatado.indexOf(":")));
+                horarioConvertido = horas * 60 + minutos;
+//                horarios.add(horarioConvertido);
+                    //Se estiver dentro do intervalo permitido, incrementa contador e adiciona o horário na soma
+                if (horarioConvertido - horarioMestre < tempo || horarioConvertido - horarioMestre > tempo * -1) {
+                    contador++;
+                    soma += horarioConvertido;
+                }
+//                }
+
+                //se contador for zero(não foi recuperado nenhum relógio ou todos estão fora do limite, média fica zero
+                media = contador == 0 ? 0 : soma / contador;
+                int valorEnviado;
+                //Calcula valores de correção (Média de diferença - diferença entre horario escravo e horario mestre
+//                for (int i = 0; i < sockets.size(); i++) {
+                valorEnviado = media - (horarioConvertido - horarioMestre);
+                sendData = ByteBuffer.allocate(4).putInt(valorEnviado).array();
+                    //Manda para as outras máquinas ressincronizarem seus relógios
+                socket.send(new DatagramPacket(sendData, sendData.length, ip, porta));
+//                }
+            }
+        } catch (SocketException ex) {
+            Logger.getLogger(Mestre.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Mestre.class.getName()).log(Level.SEVERE, null, ex);
         }
-        //se contador for zero(não foi recuperado nenhum relógio ou todos estão fora do limite, média fica zero
-        media = contador==0 ? 0 : soma/contador;
-        
-        //Calcula valores de correção (Média de diferença - diferença entre horario escravo e horario mestre
-        for(int i =0; i < horarios.length; i++){
-            horarios[i] = media - (horarios[i] - horarioMestre);
-        }
-        //Manda para as outras máquinas ressincronizarem seus relógios
-        
-        
     }
 }
